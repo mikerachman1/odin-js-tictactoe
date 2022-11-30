8ae012fd6a78ae6903c5a50714c5da37c18e9cf4
Mike Rachman <mikerachman1@gmail.com>
2022-11-30 10:47:50 -0500

    wrote gameboard module

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+// gameboard module
+const Gameboard = (function () {
+  let boardArray = [];
+  const full = () => boardArray.length === 9 && allCellsFull(); 
+  const clearBoard = () => boardArray.splice(0, boardArray.length);
+  const markCell = () => (index, symbol) => (boardArray[index] = symbol);
+  const symbolAt = () => (index) => boardArray[index];
+  
+  function allCellsFull() {
+    for (i=0; i <= 8; i++) {
+      if (boardArray[i] === undefined) return false;
+    }
+    return true;
+  }
+  
+  return { full, clearBoard, markCell, symbolAt };
+})();
